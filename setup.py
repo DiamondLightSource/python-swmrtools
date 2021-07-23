ac1df336db8f2b7067b886abc78f10ec87ad7d41
Jacob Filik <jacob.filik@diamond.ac.uk>
2021-07-23 15:10:01 +0100

    add deploy to pypi on tag to ci (#3)
    * add deploy to pypi on tag to ci
    
    * fix yaml
    
    * add missing dep

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from distutils.core import setup
 
 setup(
-    name="swmr_tools",  # How you named your package folder (MyLib)
-    packages=["swmr_tools"],  # Chose the same as "name"
-    version="0.3",  # Start with a small number and increase it with every change you make
-    license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description="Python iterator for safely monitoring NeXus files",  # Give a short description about your library
-    author="Diamond Light Source Ltd",  # Type in your name
-    author_email="scientificsoftware@diamond.ac.uk",  # Type in your E-Mail
-    url="https://github.com/DiamondLightSource/python-swmrtools",  # Provide either the link to your github or to your website
+    name="swmr_tools",
+    packages=["swmr_tools"],
+    version="0.3",
+    license="MIT",
+    description="Python iterator for safely monitoring NeXus files",
+    author="Diamond Light Source Ltd",
+    author_email="scientificsoftware@diamond.ac.uk",
+    url="https://github.com/DiamondLightSource/python-swmrtools",
     download_url="https://github.com/DiamondLightSource/python-swmrtools/v0.3.tar.gz",
-    keywords=["HDF5", "Iterator", "Diamond"],  # Keywords that define your package best
-    install_requires=["h5py", "numpy"],  # I get to this in a second
+    keywords=["HDF5", "Iterator", "Diamond"],
+    install_requires=["h5py", "numpy"],
     classifiers=[
-        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",  # Again, pick a license
-        "Programming Language :: Python :: 3",  # Specify which pyhton versions that you want to support
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
