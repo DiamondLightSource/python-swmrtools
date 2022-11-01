f3764d80924b0eff621fe4264e12be9b30c993b9
Dean Keeble <dean.keeble@diamond.ac.uk>
2022-11-01 15:38:25 +0000

    remove refresh method from mock

--- tests/utils.py
+++ tests/utils.py
@@ -6,7 +6,6 @@ def make_mock(shape=[5, 10, 1, 1], maxshape=None):
 
     mds = Mock()
     mds.dataset = np.zeros(shape)
-    mds.refresh = lambda: print("Refresh")
 
     if maxshape is None:
         mds.maxshape = shape
