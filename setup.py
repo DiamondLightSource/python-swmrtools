4043aff60b959e03d1d391395ac58e3e61d65f60
Jacob Filik <jacob.filik@diamond.ac.uk>
2022-11-11 15:49:33 +0000

    Bump version: 0.7.4 → 0.8.0 (#44)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="swmr_tools",
     packages=["swmr_tools"],
-    version="0.7.4",
+    version="0.8.0",
     license="MIT",
     description="Python iterator for safely monitoring NeXus files",
     long_description=long_description,
