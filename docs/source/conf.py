623ef50d560898d826c30c152550b4e5891e244e
Jacob Filik <jacob.filik@diamond.ac.uk>
2023-10-26 08:21:00 +0100

    Bump version: 0.8.1 → 0.9.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Diamond Light Source Ltd"
 author = "Richard E Parke"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.1"
+release = "0.9.0"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="swmr_tools",
     packages=["swmr_tools"],
-    version="0.8.1",
+    version="0.9.0",
     license="MIT",
     description="Python iterator for safely monitoring NeXus files",
     long_description=long_description,
